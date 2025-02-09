9915a2485a7ca91201c1c6a65e76b1ffb4673600
Mihai-Cristian Farcaș <mihaifarcas125@gmail.com>
2025-02-09 10:59:28 +0200

    minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import Providers from '@/components/providers'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin']
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin']
-})
 
 export const metadata: Metadata = {
   title: 'Mihai-Cristian Farcaș',
@@ -27,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <body
-        className={`flex min-h-screen flex-col font-sans antialiased ${geistSans.variable} ${geistMono.variable}`}
-      >
+      <body className={`flex min-h-screen flex-col antialiased`}>
         <Providers>
           <Header />
           <main className='grow'>{children}</main>
