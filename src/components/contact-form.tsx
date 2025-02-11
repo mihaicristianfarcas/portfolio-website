ba4196c58ab70301b744de44931abd0d6b16e94f
Mihai-Cristian Farcaș <mihaifarcas125@gmail.com>
2025-02-11 18:41:26 +0200

    added gpt project & contact form layout fix

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -44,10 +44,10 @@ export default function ContactForm() {
     <section className='relative isolate'>
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className='mt-10 lg:flex-auto'
+        className='mt-8 lg:flex-auto'
         noValidate
       >
-        <div className='grid grid-cols-1 gap-6 sm:grid-cols-2'>
+        <div className='grid grid-cols-1 gap-4 sm:grid-cols-2'>
           {/* Name */}
           <div>
             <Input
