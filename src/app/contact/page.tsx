367a1e218068bf948348d637457b682bef26fba8
Mihai-Cristian Farcaș <mihaifarcas125@gmail.com>
2025-02-16 02:48:07 +0200

    feedback encouragement!

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -10,9 +10,18 @@ export default function Contact() {
           I&apos;m always open to new opportunities and collaborations. Feel
           free to reach out to me.
         </p>
+        <p className='text-muted-foreground'>
+          Also, if you have any feedback you&apos;d like to share, please do so,
+          I&apos;d love to hear it! :D
+        </p>
+        <p className='text-muted-foreground'>
+          Some of these reviews will be featured on my &apos;reviews&apos;
+          section. If that&apos;s something you&apos;re not comfortable with,
+          please let me know.
+        </p>
         <ContactForm />
       </div>
-      <div className='container mt-16 max-w-3xl'>
+      <div className='container mt-32 max-w-3xl'>
         <NewsletterForm />
       </div>
     </section>
