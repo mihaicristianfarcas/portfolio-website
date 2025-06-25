3439517940263b3ac1c2ece86d7ffa49693d51e2
Mihai-Cristian Farcaș <mihaifarcas125@gmail.com>
2025-06-25 10:24:51 +0300

    updated syntax highlighting

--- src/components/mdx-content.tsx
+++ src/components/mdx-content.tsx
@@ -5,12 +5,38 @@ import Quote from '@/components/quote'
 import ContentFooter from './content-footer'
 
 function Code({ children, ...props }: any) {
-  const codeHTML = highlight(children)
-  return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />
+  // For inline code
+  if (typeof children === 'string') {
+    const codeHTML = highlight(children)
+    return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />
+  }
+  return <code {...props}>{children}</code>
+}
+
+function Pre({ children, ...props }: any) {
+  // Check if this is a code block by looking for a nested code element
+  if (children?.type === 'code' && typeof children.props.children === 'string') {
+    const codeContent = children.props.children
+    
+    const codeHTML = highlight(codeContent)
+    
+    return (
+      <pre {...props}>
+        <code 
+          className={children.props.className || ''}
+          dangerouslySetInnerHTML={{ __html: codeHTML }} 
+        />
+      </pre>
+    )
+  }
+  
+  // For any other pre content, return as-is
+  return <pre {...props}>{children}</pre>
 }
 
 const components = {
   code: Code,
+  pre: Pre,
   Quote: Quote,
   ContentFooter: ContentFooter
 }
